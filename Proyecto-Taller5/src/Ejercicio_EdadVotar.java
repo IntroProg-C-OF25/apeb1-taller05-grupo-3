102d7db740105cf7f0e39438079749d64d069535
Daniel-Matute <danielmatetezs@gmail.com>
2024-11-07 11:46:14 -0500

    Proyecto-Taller5

--- Proyecto-Taller5/src/Ejercicio_EdadVotar.java
+++ Proyecto-Taller5/src/Ejercicio_EdadVotar.java
@@ -1,12 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
+/** *
+ * Edad para Votar: Determinar si una persona es elegible para votar en función de su edad minimo 18 años.
  *
- * @author UTPL
+ * @author Daniel Matute
  */
+import java.util.Scanner;
+
 public class Ejercicio_EdadVotar {
-    
+
+    public static void main(String[] args) {
+        int edad;
+        Scanner tcl = new Scanner(System.in);
+        System.out.print("Dame edad: ");
+        edad = tcl.nextInt();
+        if (edad >= 18) {
+            System.out.println("Si puede votar");
+        } else {
+            System.out.println("No puede votar");
+        }
+        /**
+         * *
+         * run: Dame edad: 50 Si puede votar
+         */
+
+    }
 }
